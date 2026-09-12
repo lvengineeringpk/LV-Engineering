3c4a6c87140e349ff343186200cc1ebd56fac26c
lvengineeringpk <lvengineering.pk@gmail.com>
2026-09-12 14:40:33 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: /LV-Engineering/',
+base: '/LV-Engineering/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
